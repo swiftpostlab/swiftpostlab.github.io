479cb01828e2f3796ef5b48c9634d71e894410de
fcole90 <fcole90@gmail.com>
2023-03-09 20:58:31 +0100

    Add test url to constants

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,3 @@
-const API_LOCAL = 'http://localhost:3001'
-export const BASE_URL = process.env.NODE_ENV === "development" ? API_LOCAL : process.env.API_URL_TEST
+const API_LOCAL_URL = 'http://localhost:3001'
+const API_TEST_URL = 'https://shy-tan-meerkat-garb.cyclic.app'
+export const BASE_URL = process.env.NODE_ENV === "development" ? API_LOCAL_URL : API_TEST_URL
