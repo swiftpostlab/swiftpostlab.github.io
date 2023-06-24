d2f678d65a706e72866acc0d552211a82c5cb700
fcole90 <fcole90@gmail.com>
2023-06-24 20:48:55 +0200

    Switch to render BE

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,3 +1,3 @@
 const API_LOCAL_URL = 'http://localhost:3001'
-const API_TEST_URL = 'https://shy-tan-meerkat-garb.cyclic.app'
+const API_TEST_URL = 'https://swiftpost-backend-test.onrender.com'
 export const BASE_URL = process.env.NODE_ENV === "development" ? API_LOCAL_URL : API_TEST_URL
